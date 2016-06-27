062d88bad53dd8dbba177241bc1f58bc0b2df597
vethastanley <vethastanley@gmail.com>
2016-06-27 17:42:25 +0530

    Mongo script

--- mongo_startup.bat
+++ mongo_startup.bat
@@ -4,5 +4,6 @@ rem         If you don't have mongo db please install.
 rem PROJECT_LOC provide the location where your project is located.
 
 set MONGO=C:\Stanley\Software\mongodb-win32-x86_64-2008plus-2.4.9\mongodb-win32-x86_64-2008plus-2.4.9\bin
-set PROJECT_LOC=C:\Users\vst\Documents\GitHub\iot
-call %MONGO%\mongod -dbpath %PROJECT_LOC%\demodb
+
+if not exist ..\demodb mkdir ..\demodb
+call %MONGO%\mongod -dbpath ..\demodb
